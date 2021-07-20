be1bff031b447d3c4542e62066927dc0a94be123
Wellington Domiciano <wldomiciano@gmail.com>
2021-07-20 19:10:53 -0300

    Update code

--- app/bot.js
+++ app/bot.js
@@ -5,6 +5,7 @@ const path = require('path');
 const client = new Client();
 const commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
+const prefix = '!';
 
 http.createServer((_, res) => res.end('Estou funcionando!')).listen(3000);
 
@@ -18,10 +19,14 @@ fs.readdirSync(commandsPath)
 client
   .once('ready', () => console.log(`Logged in as ${client.user.tag}!`))
   .on('message', async (msg) => {
-    const args = msg.content.slice('!').trim().split(/ +/);
-    const command = args.shift().toLowerCase();
+    if (
+      !msg.author.bot &&
+      msg.content.startsWith(prefix) &&
+      ['867104422671417394'].includes(msg.channel.id)
+    ) {
+      const args = msg.content.slice(prefix.length).trim().split(/ +/);
+      const command = args.shift().toLowerCase();
 
-    if (!msg.author.bot && ['867104422671417394'].includes(msg.channel.id)) {
       if (commands.has(command)) {
         try {
           commands.get(command).execute(msg, args);
