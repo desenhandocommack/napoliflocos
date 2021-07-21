ef79f2a40fdb0d27152343ebed0ef74f96b03050
Wellington Domiciano <wldomiciano@gmail.com>
2021-07-21 19:55:35 -0300

    Update code

--- app/commands/roll.js
+++ app/commands/roll.js
@@ -4,8 +4,11 @@ module.exports = {
   name: 'roll',
   description: 'Rolagem de dados',
   execute(msg, args) {
-    const parsed =
-      args && args.length > 0 ? roll(msg.content.substr(5).trim()) : roll('d6');
-    msg.reply(`${JSON.stringify(parsed.results)} (${parsed.total})`);
+    if (args && args.length > 0) {
+      const parsed = roll(msg.content.substr(5).trim());
+      msg.reply(`${JSON.stringify(parsed.results)} (${parsed.total})`);
+    } else {
+      msg.reply(roll('d6').total);
+    }
   },
 };
