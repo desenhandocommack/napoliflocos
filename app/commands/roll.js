53604f9e0df59f02d948f8163208028fe68bf39f
Wellington Domiciano <wldomiciano@gmail.com>
2021-07-21 19:52:30 -0300

    Update code

--- app/commands/roll.js
+++ app/commands/roll.js
@@ -1,11 +1,11 @@
-const diceUtils = require('dice-utils');
+const { roll } = require('dice-utils');
 
 module.exports = {
   name: 'roll',
   description: 'Rolagem de dados',
-  execute(msg) {
-    const { roll } = diceUtils;
-    const parsed = roll(msg.content.substr(5).trim());
+  execute(msg, args) {
+    const parsed =
+      args && args.length > 0 ? roll(msg.content.substr(5).trim()) : roll('d6');
     msg.reply(`${JSON.stringify(parsed.results)} (${parsed.total})`);
   },
 };
